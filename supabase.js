dcca0713d509bea6d7759724736d42b4d2f6b84f
mustofacoo <96123442+mustofacoo@users.noreply.github.com>
2026-01-14 13:31:36 +0700

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -3,8 +3,8 @@
 // 1. Dapatkan URL dan Kunci 'anon' dari Pengaturan Supabase Anda
 // Masuk ke Supabase > Proyek Anda > Settings (roda gigi) > API
 
-const SUPABASE_URL = 'https://tcaiuzhcrywzkzdyytod.supabase.co'; //
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjYWl1emhjcnl3emt6ZHl5dG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NDExMTMsImV4cCI6MjA4MDMxNzExM30.mfKBVMD-wyS4S05GX6srUR5WWa1pDnq6f4eQ34-SPRE'; // 
+const SUPABASE_URL = 'https://vosaiialqhrtdaenglhp.supabase.co'; //
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvc2FpaWFscWhydGRhZW5nbGhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNzE5NDYsImV4cCI6MjA4Mzk0Nzk0Nn0.lVBsrYaL__XslvQRdNo1820La09BdmE-c8aozSekDYA'; // 
 
 // Inisialisasi Supabase Client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
